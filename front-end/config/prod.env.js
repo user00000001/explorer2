e42b59b611e9eec929ce3ea63d48a27db7ba18b2
wujiang <wujiang_dykz@163.com>
2019-12-06 10:26:42 +0800

    修改浏览器映射地址

--- front-end/config/prod.env.js
+++ front-end/config/prod.env.js
@@ -4,11 +4,11 @@ module.exports = {
   NODE_ENV: '"production"',
 
   // 单独提供给addresses页面的接口，golang实现的
-  EXPLORE_URL: '"https://explorer.ont.io/"',
+  EXPLORE_URL: '"https://localhost/"',
   TEST_EXPLORE_URL: '"https://polarisexplorer.ont.io/"',
 
   // 基础API接口
-  API_URL: '"https://explorer.ont.io/api/v1/explorer"',
+  API_URL: '"https://localhost:8585/api/v1/explorer"',
   TEST_API_URL: '"https://polarisexplorer.ont.io/api/v1/explorer"',
 
   // 提供节点统计信息计算查询的接口：
