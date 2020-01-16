a5b9c1a73956434c7c296c9d965baa798630202d
jiangyunbin <jiangyunbin@tesra.vip>
2020-01-16 11:19:40 +0800

    rename statistics to Statistics

--- back-end-projects/statistics/src/main/java/com/github/TesraSupernet/explorer/statistics/mapper/ConfigMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.TesraSupernet.explorer.statistics.mapper;
-
-import com.github.TesraSupernet.explorer.statistics.model.Config;
-import org.springframework.stereotype.Repository;
-import tk.mybatis.mapper.common.Mapper;
-
-@Repository
-public interface ConfigMapper extends Mapper<Config> {
-}
